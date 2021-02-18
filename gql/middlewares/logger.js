5747a602fa2319be4a87c25aee14f8c4c741a926
Levi Wheatcroft <levi@wheatcroft.com.au>
2021-02-18 19:18:16 +0800

    improve query logger

--- gql/middlewares/logger.js
+++ gql/middlewares/logger.js
@@ -2,10 +2,12 @@ async function logger (resolve, root, args, ctx, info) {
   const {
     fieldName
   } = info
-  console.log(fieldName)
-  console.log(ctx.req.body)
+  console.log(`operation: ${ctx.req.body.operationName}`)
+  console.log(ctx.req.body.variables)
 
   const result = await resolve(root, args, ctx, info)
+
+  console.log(result)
 
   return result
 }
