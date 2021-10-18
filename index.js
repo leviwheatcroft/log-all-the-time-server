af37a1e2da913c05b98c4e05b5ce1952da4e5389
Levi Wheatcroft <levi@wheatcroft.com.au>
2021-10-19 04:21:42 +0800

    serve static assets

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv-flow').config()
+const express = require('express')
 const {
   info
 } = require('./lib/log')
@@ -9,13 +10,29 @@ const {
   dbInitialised
 } = require('./db')
 
-Promise.all([apolloListen(), dbInitialised]).then(([{ url }]) => {
-  if (process.env.NODE_ENV !== 'production')
-    info(`🚀  GraphQL ready at ${url}`)
-  else
-    info('🚀  GraphQL server listening')
-})
-
-// dbInitialised.then(() => {
-//   info('🚀 db ready')
+// Promise.all([apolloListen(), dbInitialised]).then(([{ url }]) => {
+//   if (process.env.NODE_ENV !== 'production')
+//     info(`🚀  GraphQL ready at ${url}`)
+//   else
+//     info('🚀  GraphQL server listening')
 // })
+
+async function startServer () {
+  const app = express()
+
+  app.use(express.static(process.env.CLIENT_PATH))
+
+  const {
+    url,
+    server
+  } = await apolloListen()
+
+  server.applyMiddleware({ app })
+  await Promise.all([
+    new Promise((resolve) => app.listen({ port: 4000 }, resolve)),
+    dbInitialised
+  ])
+  info(`🚀  GraphQL ready at ${url}${server.graphqlPath}`)
+}
+
+startServer()
